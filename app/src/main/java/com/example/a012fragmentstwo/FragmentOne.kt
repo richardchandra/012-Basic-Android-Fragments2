f9fa4eb7ca0deaa9a8f89126135a5227b9fc265a
unknown <richardchandra99@gmail.com>
2022-03-16 16:52:10 +0700

    Initial commit for learning fragments

---
+++ app/src/main/java/com/example/a012fragmentstwo/FragmentOne.kt
@@ -0,0 +1,42 @@
+package com.example.a012fragmentstwo
+
+import android.content.Context
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentOne:Fragment(){
+
+    val TAG = "Fragment One"
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater!!.inflate(R.layout.fragment_one, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
